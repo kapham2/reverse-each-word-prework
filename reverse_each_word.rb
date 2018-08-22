0a81feb21f2e783a6f2c51b05a4c23fee4a6aa1b
Kim Pham <pham.kim.a@gmail.com>
2018-08-22 18:47:37 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(string)
+  array = string.split
+  reversed_array = []
+  
+#  array.each do |word|
+  array.collect do |word|
+    reversed_array.push(word.reverse)
+  end
+  
+  reversed_array.join(" ")
+end
